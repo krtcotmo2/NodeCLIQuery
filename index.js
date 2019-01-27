9c4baf153af5449e9fd95309ebe9bde5f6772a73
Kurt Cooney <krtcotmo2@gmail.com>
2019-01-27 17:10:51 -0500

    Completed the movie api call

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //js
+let liri = require("./liri.js");
+let command = process.argv[2];
+let request = process.argv[3];
+
+switch (command) {
+     case `concert-this`:
+          liri.getConcert(request);
+          break;
+     case `spotify-this-song`:
+          liri.getSong(request);
+          break;
+     case `movie-this`:
+          liri.getMovie(request);
+          break;
+     case `do-what-it-says`:
+          liri.getAction(request);
+          break;
+     default:
+          break;
+}
