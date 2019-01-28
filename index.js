a9af30d4236fdf383f4d55a95e935a4683e10b87
Kurt Cooney <krtcotmo2@gmail.com>
2019-01-27 21:31:23 -0500

    Added logging files

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 let liri = require("./liri.js");
 let command = process.argv[2];
 let request = process.argv[3];
-
+let logFile = "log.txt";
 switch (command) {
      case `concert-this`:
-          liri.getConcert(request);
+          liri.getConcert(request, logFile);
           break;
      case `spotify-this-song`:
-          liri.getSong(request);
+          liri.getSong(request, logFile);
           break;
      case `movie-this`:
           if(request == null){
                request = "Mr.Nobody";
           }
-          liri.getMovie(request);
+          liri.getMovie(request, logFile);
           break;
      case `do-what-it-says`:
-          liri.getAction(request);
+          liri.getAction(request, logFile);
           break;
      default:
           break;
